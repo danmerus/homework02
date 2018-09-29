e633100f51bbf20a2c83e9b4b6eb3c5444365bbe
danmerus <leoghost@mail.ru>
2018-09-29 14:28:26 +0500

    v11

--- src/main/scala/fintech/homework02/ComplexNumber.scala
+++ src/main/scala/fintech/homework02/ComplexNumber.scala
@@ -1,10 +1,38 @@
 package fintech.homework02
 
-class ComplexNumber {
-  ???
+case class ComplexNumber(re:Int, im:Int) {
 
-  // Написать класс описывающий комплексные числа.
-  // Реализовать проверку на равенство, умножение и сложение, toString.
-  // Реализовать оператор возведения в целую степень: "~".
-  // Реализовать тесты в ComplexNumberSpec
+  def +(n2: ComplexNumber): ComplexNumber ={
+    ComplexNumber(this.re + n2.re, this.im + n2.im)
+  }
+
+  def -(n2: ComplexNumber): ComplexNumber ={
+    ComplexNumber(this.re - n2.re, this.im - n2.im)
+  }
+
+  def *(n2: ComplexNumber): ComplexNumber ={
+    ComplexNumber(this.re * n2.re - this.im * n2.im,
+      this.im*n2.re + this.re * n2.im)
+  }
+
+  def ~(power: Int): ComplexNumber ={
+    var res = this
+    var count = power
+    while (count > 1) {
+      res = res*this
+      count -= 1
+    }
+    res
+  }
+
+  override def equals(complex: Any): Boolean = {
+    complex match {
+      case c:ComplexNumber => c.im == this.im && c.re == this.re
+      case _ => false
+    }
+  }
+
+  override def toString: String = {
+    this.re + " + " + this.im + "i"
+  }
 }
--- src/test/scala/fintech/homework02/ComplexNumberSpec.scala
+++ src/test/scala/fintech/homework02/ComplexNumberSpec.scala
@@ -1,6 +1,50 @@
 package fintech.homework02
+
 import org.scalatest.{FlatSpec, Matchers}
 
 class ComplexNumberSpec extends FlatSpec with Matchers {
-  ???
+
+  "Complex numbers" should "be added correctly" in {
+    val c1 = ComplexNumber(2, 3)
+    val c2 = ComplexNumber(1, 2)
+    c1 + c2 shouldEqual ComplexNumber(3, 5)
+  }
+
+  "Complex numbers" should "be substracted correctly" in {
+    val c1 = ComplexNumber(2, 3)
+    val c2 = ComplexNumber(1, 2)
+    c1 - c2 shouldEqual ComplexNumber(1, 1)
+  }
+
+  "Complex numbers" should "be multiplied correctly" in {
+    val c1 = ComplexNumber(2, 3)
+    val c2 = ComplexNumber(1, 2)
+    c1 * c2 shouldEqual ComplexNumber(-4, 7)
+  }
+
+  "Complex numbers" should "be raised to a power correctly" in {
+    val c1 = ComplexNumber(2, 3)
+    c1~4 shouldEqual ComplexNumber(-119, -120)
+  }
+
+  "Complex numbers" should "be compared correctly1" in {
+    val c1 = ComplexNumber(2, 3)
+    val c2 = ComplexNumber(2, 3)
+    c1 == c2 should be (true)
+  }
+
+  "Complex numbers" should "be compared correctly2" in {
+    val c1 = ComplexNumber(2, 3)
+    val c2 = ComplexNumber(1, 3)
+    c1 == c2 should be (false)
+  }
+
+  "Complex numbers" should "be compared  correctly3" in {
+    val c1 = ComplexNumber(2, 3)
+    val c2 = "234"
+    c1 == c2 should be (false)
+  }
+
+
 }
+
