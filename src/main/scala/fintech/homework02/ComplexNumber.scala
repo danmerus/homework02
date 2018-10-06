498b7bbcb1626ddca82e5c63b2cc0ac33f1fcb39
danmerus <leoghost@mail.ru>
2018-10-06 10:33:56 +0500

    removed case keyword and more

--- src/main/scala/fintech/homework02/ComplexNumber.scala
+++ src/main/scala/fintech/homework02/ComplexNumber.scala
@@ -1,22 +1,23 @@
 package fintech.homework02
 
-case class ComplexNumber(re:Double, im:Double) {
+class ComplexNumber(val re:Double, val im:Double) {
 
-  def +(n2: ComplexNumber): ComplexNumber ={
-    ComplexNumber(this.re + n2.re, this.im + n2.im)
+
+  def +(n2: ComplexNumber): ComplexNumber = {
+    new ComplexNumber(this.re + n2.re, this.im + n2.im)
   }
 
-  def -(n2: ComplexNumber): ComplexNumber ={
-    ComplexNumber(this.re - n2.re, this.im - n2.im)
+  def -(n2: ComplexNumber): ComplexNumber = {
+    new ComplexNumber(this.re - n2.re, this.im - n2.im)
   }
 
-  def *(n2: ComplexNumber): ComplexNumber ={
-    ComplexNumber(this.re * n2.re - this.im * n2.im,
+  def *(n2: ComplexNumber): ComplexNumber = {
+    new ComplexNumber(this.re * n2.re - this.im * n2.im,
       this.im*n2.re + this.re * n2.im)
   }
 
-  def c(n2: ComplexNumber): ComplexNumber ={
-    ComplexNumber(n2.re, -1*n2.im)
+  def c(n2: ComplexNumber): ComplexNumber = {
+    new ComplexNumber(n2.re, -1*n2.im)
   }
 
   def ~(power: Int): ComplexNumber ={
@@ -35,18 +36,20 @@ case class ComplexNumber(re:Double, im:Double) {
         count -= 1
       }
       val divider = (res * c(res)).re
-      res = ComplexNumber(res.re/divider, res.im/divider)
+      res = new ComplexNumber(res.re/divider, res.im/divider)
     }
     res
   }
 
-   def =~=(complex: Any): Boolean = {
+   override def equals(complex: Any): Boolean = {
     complex match {
       case c:ComplexNumber => Math.abs(c.im - this.im) < 0.00000001 &&
         Math.abs(c.re - this.re) < 0.00000001
       case _ => false
     }
   }
+
+  override def hashCode(): Int = super.hashCode()
 
   override def toString: String = {
     this.re + " + " + this.im + "i"
@@ -56,9 +59,10 @@ case class ComplexNumber(re:Double, im:Double) {
   object test {
 
     def main(args: Array[String]): Unit = {
-      var c1 = ComplexNumber(2, 3)
-      var c2 = ComplexNumber(1, -2)
+      var c1 = new ComplexNumber(2, 3)
+      var c2 = new ComplexNumber(1, -2)
       println(c1~(-2))
+      println(List(1, "one"))
     }
   }
 
--- src/test/scala/fintech/homework02/ComplexNumberSpec.scala
+++ src/test/scala/fintech/homework02/ComplexNumberSpec.scala
@@ -5,40 +5,48 @@ import org.scalatest.{FlatSpec, Matchers}
 class ComplexNumberSpec extends FlatSpec with Matchers {
 
   "Complex numbers" should "be added correctly" in {
-    val c1 = ComplexNumber(2, 3)
-    val c2 = ComplexNumber(1, 2)
-    c1 + c2 shouldEqual ComplexNumber(3, 5)
+    val c1 = new ComplexNumber(2, 3)
+    val c2 = new ComplexNumber(1, 2)
+    c1 + c2 shouldEqual new ComplexNumber(3, 5)
   }
 
   "Complex numbers" should "be substracted correctly" in {
-    val c1 = ComplexNumber(2, 3)
-    val c2 = ComplexNumber(1, 2)
-    c1 - c2 shouldEqual ComplexNumber(1, 1  )
+    val c1 = new ComplexNumber(2, 3)
+    val c2 = new ComplexNumber(1, 2)
+    c1 - c2 shouldEqual new ComplexNumber(1, 1  )
   }
 
   "Complex numbers" should "be multiplied correctly" in {
-    val c1 = ComplexNumber(2, 3)
-    val c2 = ComplexNumber(1, 2)
-    c1 * c2 shouldEqual ComplexNumber(-4, 7)
+    val c1 = new ComplexNumber(2, 3)
+    val c2 = new ComplexNumber(1, 2)
+    c1 * c2 shouldEqual new ComplexNumber(-4, 7)
   }
 
   "Complex numbers" should "be raised to a power correctly" in {
-    val c1 = ComplexNumber(2, 3)
-    c1~4 shouldEqual ComplexNumber(-119, -120)
+    val c1 = new ComplexNumber(2, 3)
+    c1~4 shouldEqual new ComplexNumber(-119, -120)
   }
 
   "Complex numbers" should "be compared correctly1" in {
-    var c1 = ComplexNumber(1, 2)
-    var c2 = ComplexNumber(1, 1)
-    var c3 = ComplexNumber(2, 3.00000000001)
+    var c1 = new ComplexNumber(1, 2)
+    var c2 = new ComplexNumber(1, 1)
+    var c3 = new ComplexNumber(2, 3.00000000001)
     var c4 = c3 - c2
-    c4 =~= c1 should be (true)
+    c4 == c1 should be (true)
   }
 
   "Complex numbers" should "be compared correctly2" in {
-    val c1 = ComplexNumber(2, 3)
-    val c2 = ComplexNumber(1, 3)
-    c1 =~= c2 should be (false)
+    var c1 = new ComplexNumber(1, 2)
+    var c2 = new ComplexNumber(1, 1)
+    var c3 = new ComplexNumber(2, 3.0001)
+    var c4 = c3 - c2
+    c4 == c1 should be (false)
+  }
+
+  "Complex numbers" should "be compared correctly3" in {
+    val c1 = new ComplexNumber(2, 3)
+    val c2 = new ComplexNumber(1, 3)
+    c1 == c2 should be (false)
   }
 
 }
